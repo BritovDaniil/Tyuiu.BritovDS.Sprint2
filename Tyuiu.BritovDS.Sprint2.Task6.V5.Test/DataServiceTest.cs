ec96a5202947cb083273bdb976f94fdac77b0279
user <user@WIN-FJ5S0KDD7IV>
2025-11-23 12:19:45 +0300

    Task6

---
+++ Tyuiu.BritovDS.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BritovDS.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.BritovDS.Sprint2.Task6.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestFindCardValue()
+        {
+            DataService ds = new DataService();
+            int value = 6;
+            string res = ds.FindCardValue(value);
+            string wait = "шестёрка";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
