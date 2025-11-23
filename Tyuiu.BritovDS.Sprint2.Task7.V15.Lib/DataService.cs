7da1c5f6334abac6fea30bc4d4a311ae38d71b1c
user <user@WIN-FJ5S0KDD7IV>
2025-11-23 12:29:22 +0300

    Task7

---
+++ Tyuiu.BritovDS.Sprint2.Task7.V15.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BritovDS.Sprint2.Task7.V15.Lib
+{
+    public class DataService : ISprint2Task7V15
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((x * x + y * y >= 1) && (x * x + y * y <= 2 * 2))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.BritovDS.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BritovDS.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.BritovDS.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService service1 = new DataService();
+            Assert.AreEqual(true, service1.CheckDotInShadedArea(1.2, -1.2));
+        }
+    }
+}
